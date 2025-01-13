78ca52b9f5048a325897bd00861deaef18cb5908
madalin777 <115221843+madalin777@users.noreply.github.com>
2025-01-13 23:57:15 +0200

    Add files via upload

--- p.asm
+++ p.asm
@@ -5,20 +5,30 @@ code segment
 ASSUME CS:code, DS:data
 
 data segment
-    welcome_msg db '	Bine Ati Venit', 0Dh, 0Ah, '$' ; Mesajul de bun venit
+    welcome_msg db '	Bine Ati Venit', 0Dh, 0Ah, '$'
     optiune_msg db 'Selectati o Optiune:$'
     op_1 db '1.Intrare In Parcare$'
     op_2 db '2.Iesire din Parcare$'
     op_3 db '3.Configurarea Sistemului$'
     op_4 db '4.Anulare$'
-	optiune db 0 ; Variabila pentru a salva opțiunea aleasă
+    optiune db 0 ; Variabila pentru a salva opțiunea aleasă
     msg_1 db 'Intrare in Parcare.$'
     msg_2 db 'Iesire din Parcare.$'
     msg_3 db 'Configurarea Sistemului.$'
     msg_4 db 'O zi buna!$'
-	msg_5 db '	Optiune invalida!$'
-	nr_inmatriculare db 0 ; Variabila pentru a salva numerele de inmatriculare
-	
+    msg_5 db '	Optiune invalida!$'
+    msg_nivel db 'Selectati nivelul (1-3):$'
+    msg_locuri db 'Locuri disponibile pe nivel:$'
+    msg_ocupat db 'Nu mai sunt locuri disponibile pe acest nivel.$'
+    msg_timer db 'Ora de intrare a fost inregistrata.$'
+    msg_eroare_nivel db 'Nivel invalid! Selectati din nou.$'
+    total_locuri db 5, 5, 5 ; Locuri totale pentru 3 niveluri
+    locuri_disponibile db 5, 5, 5 ; Locuri disponibile pentru 3 niveluri
+    ora_intrare db 0 ; Ora de intrare va fi stocată aici
+    nr_inmatriculare db 8 dup(0) ; Variabilă pentru numărul de înmatriculare
+	nr_inmatriculare_msg db 'Introduceți numărul de înmatriculare:$'
+    valid_number_msg db 'Numărul de înmatriculare este valid.$'
+    invalid_format_msg db 'Format greșit! Introduceți din nou.$'
 data ends
 
 start:
@@ -103,162 +113,196 @@ start:
         ; Compară valoarea introdusă și afișează mesajul corespunzător
         mov al, [optiune]  ; Încărcăm valoarea introdusă în AL
         
-        cmp al, '1'        
-        je intrare        
-        
-        cmp al, '2'        
-        je iesire        
-        
-        cmp al, '3'        
-        je configurare        
-        
-        cmp al, '4'        
-        je anulare        
-        
-        ; Mesajul de eroare
-        mov ah, 09h
-        lea dx, msg_5
-        int 21h
-        
-        ; Continuăm să cerem o nouă opțiune
-        jmp citire_optiune
+        cmp al, '1'
+je intrare_intermediar
+
+cmp al, '2'
+je iesire_intermediar
+
+cmp al, '3'
+je configurare_intermediar
+
+cmp al, '4'
+je anulare_operatiune
+
+jmp eroare_optiune
+
+intrare_intermediar:
+jmp intrare_parcare
+
+iesire_intermediar:
+jmp iesire_parcare
+
+configurare_intermediar:
+jmp configurare_sistem
+
+eroare_optiune:
+mov ah, 09h
+lea dx, msg_5
+int 21h
+jmp citire_optiune
+
+intrare_parcare:
+; Logica pentru intrarea în parcare
+jmp terminare_intrare
+
+iesire_parcare:
+; Logica pentru ieșirea din parcare
+jmp terminare_iesire
+
+configurare_sistem:
+; Logica pentru configurarea sistemului
+jmp terminare_configurare
+
+anulare_operatiune:
+; Logica pentru anulare
+jmp terminare_anulare
+
+terminare_intrare:
+ret
+
+terminare_iesire:
+ret
+
+terminare_configurare:
+ret
+
+terminare_anulare:
+ret
+
 
 intrare:
+    ; Afișăm mesajul pentru intrare în parcare
     mov ah, 09h
     lea dx, msg_1
     int 21h
-	; Introducere numar de inmatriculare
-intrare_numar:
-    lea dx, 'Introduceti numarul de inmatriculare (ex: BV30MAD): $'
+
+    ; Introducerea numărului de înmatriculare
     mov ah, 09h
-    int 21h
+    lea dx, nr_inmatriculare_msg
+    int 21h ; Afișăm mesajul de introducere a numărului de înmatriculare
 
-    lea dx, nr_inmatriculare
-    mov ah, 0Ah
-    int 21h
+introducere_numar:
+    lea si, nr_inmatriculare ; Pregătim locația pentru stocare
+    mov cx, 8 ; Numărul total de caractere pentru validare: 2 litere + 2 cifre + 3 litere + 1 spațiu
+    xor di, di ; Resetăm indexul pentru nr_inmatriculare
 
-    ; Validare format numar de inmatriculare
+citim_caracter:
+    mov ah, 01h ; Citim câte un caracter
+    int 21h
+    cmp al, 0Dh ; Verificăm dacă utilizatorul a apăsat Enter
+    je validare_format
+    stosb ; Salvăm caracterul citit în memorie
+    inc di ; Incrementăm indexul
+    loop citim_caracter
+
 validare_format:
-    lea si, nr_inmatriculare+1 ; Ignoram primul byte (lungimea inputului)
-    mov cl, byte ptr [nr_inmatriculare+1] ; Lungimea efectiva a inputului
-    cmp cl, 7 ; Verificam daca lungimea este exact 7 caractere
-    jne format_invalid
-
-    ; Verificam primele 2 caractere (litere mari)
-    mov al, [si]
-    cmp al, 'A'
-    jb format_invalid
-    cmp al, 'Z'
-    ja format_invalid
+    lea si, nr_inmatriculare ; Pregătim să verificăm formatul
+    mov al, [si] ; Primul caracter
+    jz format_gresit ; Dacă nu este literă, afișăm eroarea
 
     inc si
-    mov al, [si]
-    cmp al, 'A'
-    jb format_invalid
-    cmp al, 'Z'
-    ja format_invalid
+    mov al, [si] ; Al doilea caracter
+    jz format_gresit ; Dacă nu este literă, afișăm eroarea
 
     inc si
-    ; Verificam urmatoarele 2 caractere (cifre)
-    mov al, [si]
-    cmp al, '0'
-    jb format_invalid
-    cmp al, '9'
-    ja format_invalid
+    mov al, [si] ; Al treilea caracter (spațiu)
+    cmp al, ' '
+    jne format_gresit ; Dacă nu este spațiu, afișăm eroarea
 
     inc si
-    mov al, [si]
-    cmp al, '0'
-    jb format_invalid
-    cmp al, '9'
-    ja format_invalid
+    mov al, [si] ; Al patrulea caracter (cifră 1)
+    jz format_gresit ; Dacă nu este cifră, afișăm eroarea
 
     inc si
-    ; Verificam ultimele 3 caractere (litere mari)
-    mov al, [si]
-    cmp al, 'A'
-    jb format_invalid
-    cmp al, 'Z'
-    ja format_invalid
+    mov al, [si] ; Al cincilea caracter (cifră 2)
+    jz format_gresit ; Dacă nu este cifră, afișăm eroarea
 
     inc si
-    mov al, [si]
-    cmp al, 'A'
-    jb format_invalid
-    cmp al, 'Z'
-    ja format_invalid
+    mov al, [si] ; Al șaselea caracter (litera 1)
+    jz format_gresit ; Dacă nu este literă, afișăm eroarea
 
     inc si
-    mov al, [si]
-    cmp al, 'A'
-    jb format_invalid
-    cmp al, 'Z'
-    ja format_invalid
+    mov al, [si] ; Al șaptelea caracter (litera 2)
+    jz format_gresit ; Dacă nu este literă, afișăm eroarea
 
-    jmp selectie_nivel ; Format valid, trecem mai departe
+    inc si
+    mov al, [si] ; Al optulea caracter (litera 3)
+    jz format_gresit ; Dacă nu este literă, afișăm eroarea
 
-format_invalid:
-    lea dx, 'Format gresit! Introduceti din nou: $'
+    ; Numărul de înmatriculare este valid
     mov ah, 09h
+    lea dx, valid_number_msg
+    int 21h ; Afișăm mesaj de validare
+    jmp verificare_locuri
+
+format_gresit:
+    mov ah, 09h
+    lea dx, invalid_format_msg ; Mesaj de eroare
     int 21h
-    jmp intrare_numar
+    jmp introducere_numar
 
-    ; Anulare
-anulare:
-    lea dx, 'Operatiune anulata.$'
+verificare_locuri:
+    ; Afișăm mesajul pentru selecția nivelului
     mov ah, 09h
+    lea dx, msg_nivel
+    int 21h
+
+selectare_nivel:
+    mov ah, 01h ; Citire nivel de la tastatură
+    int 21h
+    sub al, '0' ; Convertim caracterul citit în număr (1 -> nivel 1)
+    cmp al, 1
+    jl eroare_nivel
+    cmp al, 3
+    jg eroare_nivel
+
+    mov bl, al ; Salvăm nivelul selectat în BL (1-3)
+    dec bl ; Convertim nivelul pentru indexare (0-2)
+    
+    ; Verificăm locurile disponibile
+    lea si, locuri_disponibile
+    mov al, [si + bx]
+    cmp al, 0 ; Verificăm dacă mai sunt locuri disponibile
+    je nivel_ocupat
+
+    ; Reducem locurile disponibile
+    dec byte ptr [si + bx]
+
+    ; Afișăm mesajul cu locuri disponibile
+    mov ah, 09h
+    lea dx, msg_locuri
+    int 21h
+    mov ah, 02h
+    add al, '0' ; Convertim numărul în caracter
+    mov dl, al
+    int 21h
+
+    ; Pornim timerul
+    lea si, ora_intrare
+    mov byte ptr [si], 10 ; Simulăm ora de intrare (ex. 10:00)
+    mov ah, 09h
+    lea dx, msg_timer
     int 21h
     jmp terminare_intrare
 
-; Selectie nivel (Verifică dacă sunt locuri disponibile)
-selectie_nivel:
-    lea si, locuri_totale
-    lea di, locuri_ocupate
-    mov cx, 4
-nivel_loop:
-    mov al, [si]
-    sub al, [di]
-    cmp al, 0
-    jg loc_disponibil
-    add si, 1
-    add di, 1
-    loop nivel_loop
-
-    lea dx, 'Ne pare rau, nu sunt locuri disponibile.$'
+nivel_ocupat:
     mov ah, 09h
+    lea dx, msg_ocupat
     int 21h
-    jmp anulare_intrare
+    jmp selectare_nivel
 
-loc_disponibil:
-    lea dx, 'Selectati nivelul (1-4): $'
+eroare_nivel:
     mov ah, 09h
+    lea dx, msg_eroare_nivel
     int 21h
+    jmp selectare_nivel
 
-    mov ah, 01h
-    int 21h
-    sub al, '1'
-    mov bl, al
-    mov al, [locuri_ocupate+bx]
-    inc al
-    mov [locuri_ocupate+bx], al
-
-    lea dx, 'Loc rezervat cu succes.$'
-    mov ah, 09h
-    int 21h
-    jmp terminare_intrare
-
-anulare_intrare:
-    lea dx, 'Operatiune anulata.$'
-    mov ah, 09h
-    int 21h
-
-terminare_intrare:
-    ; Pornește Timerul (Salvează ora la care a intrat)
-    lea dx, 'Ora de intrare a fost salvata.$'
-    mov ah, 09h
-    int 21h
-
-    jmp terminare
+terminare:
+    ret
+	
+	jmp terminare
+	
 
 iesire:
     mov ah, 09h
@@ -297,7 +341,7 @@ anulare:
 	
     jmp terminare
 
-terminare:
+
     mov ah, 4ch ; Finalizează programul
     int 21h 
 
